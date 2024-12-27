c975940a0a879b931bac3ce20e473efdfda50b64
Josh Manders <josh@joshmanders.com>
2024-12-26 19:20:41 -0600

    add relative_path helper

--- src/helpers.php
+++ src/helpers.php
@@ -1,16 +1,16 @@
 <?php
 
-// if (!function_exists('example')) {
-//   /**
-//    * Example helper function.
-//    *
-//    * @param  string|null  $foo
-//    * @param  \Illuminate\Contracts\Support\Arrayable|array  $bar
-//    * @return    * @return void
+if (! function_exists('relative_path')) {
+    /**
+     * Get the relative path from the base path.
+     *
+     * @param  string  $separator
+     */
+    function relative_path(string $path, $separator = DIRECTORY_SEPARATOR): string
+    {
+        $base = array_slice(explode($separator, rtrim(base_path(), $separator)), 1);
+        $path = array_slice(explode($separator, rtrim($path, $separator)), 1);
 
-//    */
-//   function example($foo = null, $bar = []): void
-//   {
-//     // ...
-//   }
-// }
+        return implode($separator, array_slice($path, count($base)));
+    }
+}
